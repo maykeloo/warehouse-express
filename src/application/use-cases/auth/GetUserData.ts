158d52fde15081e0a60c371c58f4c21023aca64a
Michał Sukiennik <michal.sukiennik.contact@gmail.com>
2024-06-01 17:48:47 +0200

    feat: add token expired error

--- src/application/use-cases/auth/GetUserData.ts
+++ src/application/use-cases/auth/GetUserData.ts
@@ -1,5 +1,5 @@
 import { IUserRepository } from '@/application/interfaces/IUserRepository';
-import jwt from 'jsonwebtoken';
+import jwt, { TokenExpiredError } from 'jsonwebtoken';
 import { Admin, Client } from '@prisma/client';
 
 export class GetUserData {
@@ -17,7 +17,31 @@ export class GetUserData {
                 messages: [],
             };
         }
-        const userData = jwt.verify(token, process.env.JWT_SECRET as string) as Client | Admin;
-        return this.userRepository.getUserById(userData.id);
+        try {
+            const userData = jwt.verify(token, process.env.JWT_SECRET as string) as Client | Admin;
+            return this.userRepository.getUserById(userData.id);
+        } catch (error) {
+            if (error instanceof TokenExpiredError) {
+                throw {
+                    errors: [
+                        {
+                            path: 'token',
+                            message: 'TOKEN_EXPIRED',
+                        },
+                    ],
+                    messages: [],
+                };
+            }
+
+            throw {
+                errors: [
+                    {
+                        path: 'token',
+                        message: 'INVALID_TOKEN',
+                    },
+                ],
+                messages: [],
+            };
+        }
     }
 }
